1ffdd1da1a190291b0f0ef9ae44413466b9ee19a
huanxi <1355473748@qq.com>
2019-03-29 01:19:35 +0800

    杨辉三角

--- Test/SeqQueueTest.cpp
+++ Test/SeqQueueTest.cpp
@@ -9,7 +9,7 @@
 void YangHuiTriangle(int i);
 
 void RunYangHuiTriangleTest() {
-    YangHuiTriangle(2);
+    YangHuiTriangle(6);
 }
 
 /**
@@ -17,21 +17,36 @@ void RunYangHuiTriangleTest() {
  *   1 1
  *  1 2 1
  * 1 3 3 1
- *
+ *第n行有n个数，
+ * 两边是1.
+ * 中间的某个数，等于上一行的两个相邻数之和
  * @param row
  */
 void YangHuiTriangle(int row) {
     SqQueue queue;
     int x, y;
     SQ_Initiate(&queue);
-    for (int i = 0; i < row; ++i) { //行循环
-        SQ_In(&queue, 1);
+    SQ_In(&queue, 1); //第一行的1
+    for (int i = 2; i <= row; ++i) { //行循环
+        SQ_In(&queue, 1); //第i行的首1
         //出队输出
-        for (int k = 0; k <= i; ++k) {
+        for (int k = 1; k <= i-2; ++k) { //列循环 打印前一行
+            //获取第i行的中间元素
+            SQ_Head(queue, &y);
             SQ_Out(&queue, &x);
-            printf("%d ", x);
+            printf("%d ", y);
+            SQ_Head(queue, &x);
+            SQ_In(&queue, x + y);//入队次数i-1
         }
-        printf("\n");
+        SQ_Head(queue, &y);
+        SQ_Out(&queue,&x);
+        printf("%d \n",y);
+        SQ_In(&queue, 1); //第i行的尾1
+    }
+    //输入最后一行
+    while(!SQ_IsEmpty(queue)){
+        SQ_Out(&queue,&x);
+        printf("%d ",x);
     }
 }
 
